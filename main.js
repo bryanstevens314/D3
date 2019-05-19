6415412ad3252658a54cb753c88d0988416d6bb8
Bryan Stevens <bryan_stevens314@yahoo.com>
2019-05-19 08:14:09 -0700

    Updated main.js
    Shortened code and removed logs

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
-
 function start (){
     d3.selectAll("p").style("background-color", function() {
         return "hsl(" + Math.random() * 360 + ",100%,50%)";
       }).style("height", function() {
-        let num = Math.random() * 360;
-        console.log(num + 'px');
+        const num = Math.random() * 360;
         return num + 'px';
       }).style("width", function() {
-        let num = Math.random() * 360;
-        console.log(num + 'px');
+        const num = Math.random() * 360;
         return num + 'px';
       });
 }
-
 let interval = setInterval(start,'500');
 
