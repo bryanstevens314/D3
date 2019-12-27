26ad207c17300b90bcf903067a640480c393ab6d
Bryan <bryan_stevens314@yahoo.com>
2019-12-26 19:37:47 -0800

    One-liners baby

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
-function start (){
-    d3.selectAll("p").style("background-color", function() {
-        return "hsl(" + Math.random() * 360 + ",100%,50%)";
-      }).style("height", function() {
-        const num = Math.random() * 360;
-        return num + 'px';
-      }).style("width", function() {
-        const num = Math.random() * 360;
-        return num + 'px';
-      });
-}
-let interval = setInterval(start,'500');
+const interval = setInterval(() =>
+  d3.selectAll("p")
+    .style("background-color", () => `hsl(${Math.random() * 360},100%,50%)`)
+    .style("height", () => `${Math.random() * 360}px`)
+    .style("width", () => `${Math.random() * 360}px`)
+  , '500');
 
