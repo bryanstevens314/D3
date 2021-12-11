c6e41b776ea8e86a609c56ab585f04dfddf71bd2
Bryan Stevens <bryan_stevens314@yahoo.com>
2021-12-11 11:57:19 -0800

    Modernized

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-const interval = setInterval(() =>
+const sleep = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+(recursive = async () => {
   d3.selectAll("p")
     .style("background-color", () => `hsl(${Math.random() * 360},100%,50%)`)
     .style("height", () => `${Math.random() * 360}px`)
-    .style("width", () => `${Math.random() * 360}px`)
-  , '500');
+    .style("width", () => `${Math.random() * 360}px`);
 
+  await sleep(500);
+  recursive();
+})(); // IIFE - Immediately Invoked Function Expression
